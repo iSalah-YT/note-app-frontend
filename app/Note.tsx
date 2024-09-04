932150d30012cec490ef40a350f3d3c664aa098d
iSalah-YT <82966610+iSalah-YT@users.noreply.github.com>
2024-09-04 19:07:09 +0100

    frontend completed

---
+++ app/Note.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { API_URL } from '../server';
+import { useEffect, useState } from 'react';
+import NoteCard from './NoteCard';
+
+type Notes = {
+  _id: string;
+  title: string;
+  content: string;
+  updatedAt: Date;
+  createdAt: Date;
+};
+
+const Note = () => {
+  const [notes, setNotes] = useState<Notes[]>([]);
+  console.log(notes);
+
+  // fetch our Notes
+  const fetchNotes = async () => {
+    try {
+      const response = await fetch(`${API_URL}/notes`);
+      const responseData = await response.json();
+      console.log(responseData); // Logs the response data
+      setNotes(responseData.data.notes);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDelete = async (id: string) => {
+    try {
+      const response = await fetch(`${API_URL}/notes/${id}`, {
+        method: 'DELETE',
+      });
+      if (response.ok) {
+        setNotes((prevNotes) => prevNotes.filter((note) => note._id !== id));
+      } else {
+        console.error('Failed to delete note');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  return (
+    <div className="w-[80%] mx-auto mt-16">
+      <h1 className="text-4xl sm:text-5xl md:text-6xl font-medium">
+        Notes <span className="text-green-600">({notes.length})</span>
+      </h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt10 mb-10">
+        {notes.map((note) => {
+          return (
+            <div key={note._id}>
+              <NoteCard note={note} onDelete={handleDelete} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Note;
---
+++ app/NoteCard.tsx
@@ -0,0 +1,68 @@
+import Link from 'next/link';
+import { BiCalendar, BiPencil, BiTrash } from 'react-icons/bi';
+
+type Props = {
+  note: {
+    _id: string;
+    title: string;
+    content: string;
+    updatedAt: Date;
+    createdAt: Date;
+  };
+  onDelete: (id: string) => void;
+};
+
+const formatDate = (date: Date) => {
+  const dateValue = new Date(date);
+  return new Intl.DateTimeFormat('en-GB', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  }).format(dateValue);
+};
+
+const NoteCard = ({ note, onDelete }: Props) => {
+  const createDate = formatDate(note.createdAt);
+
+  let updateDate;
+  if (note.updatedAt) {
+    updateDate = formatDate(note.updatedAt);
+  }
+
+  return (
+    <div className="p-8 overflow-hidden bg-blue-900 rounded-lg relative">
+      <p className="text-sm text-yellow-300 flex items-center space-x-2 mb-4 font-semibold">
+        <span>
+          <BiCalendar />
+        </span>
+        <span>{createDate}</span>
+      </p>
+
+      <h1 className="text-3xl text-white font-bold mb-4">{note.title}</h1>
+      <p className="text-base mb-4 w-[90%] text-white text-opacity-70">
+        {note.content}
+      </p>
+
+      {note.updatedAt && (
+        <p className="text-sm text-white text-opacity-65 mb-4 font-semibold">
+          Last update: {updateDate}
+        </p>
+      )}
+
+      <Link href={`/update-note/${note._id}`}>
+        <div className="w-8 h-8 bg-orange-700 hover:bg-orange-900 transition-all duration-150 rounded-full absolute top-4 cursor-pointer ring-4 flex items-center justify-center flex-col">
+          <BiPencil className="text-white w-6 h-6" />
+        </div>
+      </Link>
+
+      <div
+        onClick={() => onDelete(note._id)}
+        className="w-8 h-8 bg-rose-700 hover:bg-rose-900 transition-all duration-150 rounded-full absolute bottom-4 cursor-pointer ring-4 flex items-center justify-center flex-col"
+      >
+        <BiTrash className="text-white w-6 h-6" />
+      </div>
+    </div>
+  );
+};
+
+export default NoteCard;
